960883cad5fd0f7b767efe16c78950597ce2038c
iam-devAlhan <alhaanahmed123@gmail.com>
2025-11-16 22:34:58 +0500

    fix: Added extension as React App with Tailwind Configured

---
+++ extension/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='px-5 py-8 items-left'>
+        <h1 className='text-gray-950 font-bold text-3xl'>Hello World with <span className='text-cyan-900 hover:text-cyan-400'>React and Tailwind CSS!</span></h1>
+      </div>
+    </>
+  )
+}
+
+export default App
